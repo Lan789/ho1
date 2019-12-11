f3aab3599112082ab2737fd69415387fdcb365b9
Lan789 <58630706+Lan789@users.noreply.github.com>
2019-12-11 21:29:03 +0400

    Add files via upload

---
+++ Ex 2.js
@@ -0,0 +1,31 @@
+var a = 3;
+var b = 14;
+ if ( a % b == 0){
+   alert ("1")
+ } else if (b % a == 0){
+  alert("1")
+  }else {
+   alert ("0")
+   }
+
+var a = 18;
+var b = 2;
+ if ( a % b == 0){
+   alert ("1")
+ } else if (b % a == 0){
+  alert("1")
+  }else {
+   alert ("0")
+   }
+
+var a = 7;
+var b = 21;
+ if ( a % b == 0){
+   alert ("1")
+ } else if (b % a == 0){
+  alert("1")
+  }else {
+   alert ("0")
+   }
+
+   
